dd42e4d70e4d4a50e72834151fd64a183aa647b7
otsuki <takashi_otsuki@outlook.com>
2019-02-03 21:17:52 +0900

    Create Attacked/Inquiry/Voted ContentBuilders.

---
+++ src/org/aiwolf/client/lib/InquiryContentBuilder.java
@@ -0,0 +1,77 @@
+/**
+ * InquiryContentBuilder.java
+ * 
+ * Copyright (c) 2019 人狼知能プロジェクト
+ */
+package org.aiwolf.client.lib;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.aiwolf.common.data.Agent;
+
+/**
+ * <div lang="ja">照会発話ビルダークラス</div>
+ * 
+ * <div lang="en">Builder class for the utterance of an inquiry.</div>
+ * 
+ * @author otsuki
+ *
+ */
+public class InquiryContentBuilder extends ContentBuilder {
+
+	/**
+	 * <div lang="ja">照会発話のためのInquiryContentBuilderを構築する</div>
+	 *
+	 * <div lang="en">Constructs a InquiryContentBuilder for the utterance of an inquiry.</div>
+	 * 
+	 * @param target
+	 *            <div lang="ja">被照会エージェント</div>
+	 *
+	 *            <div lang="en">The inquired agent.</div>
+	 * @param content
+	 *            <div lang="ja">照会事項を表す{@code Content}</div>
+	 *
+	 *            <div lang="en">{@code Content} representing the inquired matter.</div>
+	 */
+	public InquiryContentBuilder(Agent target, Content content) {
+		this(Agent.UNSPEC, target, content);
+	}
+
+	/**
+	 * <div lang="ja">照会発話のためのInquiryContentBuilderを構築する</div>
+	 *
+	 * <div lang="en">Constructs a InquiryContentBuilder for the utterance of an inquiry.</div>
+	 * 
+	 * @param subject
+	 *            <div lang="ja">照会をするエージェント</div>
+	 *
+	 *            <div lang="en">The agent who inquires.</div>
+	 * @param target
+	 *            <div lang="ja">被照会エージェント</div>
+	 *
+	 *            <div lang="en">The inquired agent.</div>
+	 * @param content
+	 *            <div lang="ja">照会事項を表す{@code Content}</div>
+	 *
+	 *            <div lang="en">{@code Content} representing the inquired matter.</div>
+	 */
+	public InquiryContentBuilder(Agent subject, Agent target, Content content) {
+		topic = Topic.OPERATOR;
+		operator = Operator.INQUIRE;
+		this.subject = subject;
+		this.target = target;
+		contentList = new ArrayList<>(Arrays.asList(content));
+	}
+
+	@Override
+	String getText() {
+		return ContentBuilder.join(" ", new String[] {
+				Agent.UNSPEC == subject ? "" : subject.toString(),
+				operator.toString(),
+				target.toString(),
+				"(" + contentList.get(0).getText() + ")"
+		}).trim();
+	}
+
+}
---
+++ src/org/aiwolf/client/lib/VotedContentBuilder.java
@@ -0,0 +1,63 @@
+/**
+ * VotedContentBuilder.java
+ * 
+ * Copyright (c) 2019 人狼知能プロジェクト
+ */
+package org.aiwolf.client.lib;
+
+import org.aiwolf.common.data.Agent;
+
+/**
+ * <div lang="ja">投票報告ビルダークラス</div>
+ * 
+ * <div lang="en">Builder class for the report of a vote.</div>
+ * 
+ * @author otsuki
+ *
+ */
+public class VotedContentBuilder extends ContentBuilder {
+
+	/**
+	 * <div lang="ja">投票報告のためのVotedContentBuilderを構築する</div>
+	 *
+	 * <div lang="en">Constructs a VotedContentBuilder for the report of a vote.</div>
+	 * 
+	 * @param target
+	 *            <div lang="ja">被投票エージェント</div>
+	 * 
+	 *            <div lang="en">The voted agent.</div>
+	 */
+	public VotedContentBuilder(Agent target) {
+		this(Agent.UNSPEC, target);
+	}
+
+	/**
+	 * <div lang="ja">投票報告のためのVotedContentBuilderを構築する</div>
+	 *
+	 * <div lang="en">Constructs a VotedContentBuilder for the report of a vote.</div>
+	 * 
+	 * @param subject
+	 *            <div lang="ja">投票をしたエージェント</div>
+	 * 
+	 *            <div lang="en">The agent who voted.</div>
+	 * @param target
+	 *            <div lang="ja">被投票エージェント</div>
+	 * 
+	 *            <div lang="en">The voted agent.</div>
+	 */
+	public VotedContentBuilder(Agent subject, Agent target) {
+		topic = Topic.VOTED;
+		this.subject = subject;
+		this.target = target;
+	}
+
+	@Override
+	String getText() {
+		return ContentBuilder.join(" ", new String[] {
+				Agent.UNSPEC == subject ? "" : subject.toString(),
+				topic.toString(),
+				target.toString()
+		}).trim();
+	}
+
+}
